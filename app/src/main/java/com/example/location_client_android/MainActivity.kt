81529405d86ece8feb13cad12b42a835cd0f7392
noah64hatfield <noah.hatfield@row64.com>
2026-08-19 14:40:01 -0600

    Redid Navigation Using XML Nav Graph

--- app/src/main/java/com/example/location_client_android/MainActivity.kt
+++ app/src/main/java/com/example/location_client_android/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.createGraph
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.fragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -51,53 +52,72 @@ class MainActivity : AppCompatActivity() {
         // FRAGMENT NAVIGATION ATTEMPT 2
         // Uses dedicated nav graph XML
 
-
-
-        // -----------------------------------------------------------------------------------
-        // FRAGMENT NAVIGATION ATTEMPT 1
-        // Attempt uses dynamic nav graph with programmatic approach
-        // Couldn't get to work
-
         /**
-         * Establish fragment navigation using Kotlin DSL.
-         *
-         * First, create a reference to the NavHostFragment, then associate it to
-         * an instance of the NavController.
-         *
-         * The NavHostFragment is hosted in res/xml/layout_activity_main.xml
-         * The id of the NavHostFragment is: nav_host_fragment
+         * Establish the NavHostFragment and the NavController.
          *
          * https://developer.android.com/guide/navigation/navcontroller
+         *
+         * This references the ID of the navigation host fragment, which is
+         * hosted within the main activity's layout.
          */
-
-        // Create a reference to the NavHostFragment with ID nav_host_fragment
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
-        // Retrieve the NavController for the selected NavHostFragment
-        val navController = navHostFragment.navController
+        val navController = navHostFragment.findNavController()
 
-        /**
-         * Simultaneously create and add a navigation graph to the NavController
-         * using the createGraph() method and add destinations.
-         *
-         * https://developer.android.com/guide/navigation/design#dsl-views
-         *
-         * https://developer.android.com/guide/navigation/design/kotlin-dsl#routes
-         */
-        navController.graph = navController.createGraph(
-            startDestination = Home
-        ) {
-            fragment<HomeFragment, Home> {}
-            fragment<LocationFragment, Location> {}
-        }
-
-        // Create an action bar based on the NavController
+        // Associate the NavController with the nav bar
         // https://developer.android.com/guide/navigation/integrations/ui#bottom_navigation
         findViewById<BottomNavigationView>(R.id.bottom_navigation)
             .setupWithNavController(navController)
 
         // -----------------------------------------------------------------------------------
+
+
+//        // -----------------------------------------------------------------------------------
+//        // FRAGMENT NAVIGATION ATTEMPT 1
+//        // Attempt uses dynamic nav graph with programmatic approach
+//        // Couldn't get to work
+//
+//        /**
+//         * Establish fragment navigation using Kotlin DSL.
+//         *
+//         * First, create a reference to the NavHostFragment, then associate it to
+//         * an instance of the NavController.
+//         *
+//         * The NavHostFragment is hosted in res/xml/layout_activity_main.xml
+//         * The id of the NavHostFragment is: nav_host_fragment
+//         *
+//         * https://developer.android.com/guide/navigation/navcontroller
+//         */
+//
+//        // Create a reference to the NavHostFragment with ID nav_host_fragment
+//        val navHostFragment =
+//            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//
+//        // Retrieve the NavController for the selected NavHostFragment
+//        val navController = navHostFragment.navController
+//
+//        /**
+//         * Simultaneously create and add a navigation graph to the NavController
+//         * using the createGraph() method and add destinations.
+//         *
+//         * https://developer.android.com/guide/navigation/design#dsl-views
+//         *
+//         * https://developer.android.com/guide/navigation/design/kotlin-dsl#routes
+//         */
+//        navController.graph = navController.createGraph(
+//            startDestination = Home
+//        ) {
+//            fragment<HomeFragment, Home>(R.id.destination_home)
+//            fragment<LocationFragment, Location> {}
+//        }
+//
+//        // Create an action bar based on the NavController
+//        // https://developer.android.com/guide/navigation/integrations/ui#bottom_navigation
+//        findViewById<BottomNavigationView>(R.id.bottom_navigation)
+//            .setupWithNavController(navController)
+//
+//        // -----------------------------------------------------------------------------------
 
 
 
