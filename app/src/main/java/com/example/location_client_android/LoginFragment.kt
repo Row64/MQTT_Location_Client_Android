096962473e67b03f13da00f6dd21073bd93a5ca6
noah64hatfield <noah.hatfield@row64.com>
2026-08-20 15:57:15 -0600

    Enabled Composables in login fragment
    Using Compose may be easier long-term

--- app/src/main/java/com/example/location_client_android/LoginFragment.kt
+++ app/src/main/java/com/example/location_client_android/LoginFragment.kt
@@ -1,6 +1,74 @@
 package com.example.location_client_android
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 
-class LoginFragment : Fragment(R.layout.layout_fragment_login) {
+class LoginFragment : Fragment( /* R.layout.layout_fragment_login */ ) {
+
+    val login = MqLogin()
+
+
+    /**
+     * Enables the use of Composables in a classic fragment.
+     * This requires the XML layout to have a ComposeView block
+     *
+     * https://developer.android.com/develop/ui/compose/migrate/interoperability-apis/compose-in-views#compose-in-fragments
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.layout_fragment_login, container, false)
+        val composeView = view.findViewById<ComposeView>(R.id.compose_view)
+        composeView.apply {
+            // Dispose of the Composition when the view's LifecycleOwner
+            // is destroyed
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                // Compose UI elements go here
+                MaterialTheme {
+
+                    Btn()
+
+
+
+
+                }
+            }
+        }
+        return view
+    }
+
+
+
+
+
+}
+
+
+
+@Composable
+fun Btn() {
+    Button(
+        onClick = {
+            println("Yay! You clicked a button. Good job.")
+        }
+    ) {
+        Spacer(Modifier.width(8.dp))
+        Text("The button")
+    }
 }
